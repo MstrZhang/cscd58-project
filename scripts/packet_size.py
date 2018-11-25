842e79b4d7783cc3c5943efca534bf8d27cba5f1
zhangke <mstr.zhang@gmail.com>
2018-11-24 19:30:33 -0500

    modify directory structure

---
+++ scripts/packet_size.py
@@ -0,0 +1,70 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+mapping = {
+    'APPLICATION': ['TELNET', 'MySQL', 'SMTP', 'SSH', 'RIP', 'DHCPv6', 'DNS', 'CVSPSERVER', 'Intel ANS probe', 'VNC', 'SSL', 'ISAKMP', 'MDNS', 'MS NLB', 'DSI', 'LLMNR', 'NTP', 'NCS', 'BOOTP', 'SRVLOC'],
+    'TRANSPORT': ['TCP', 'UDP', 'NBSS', 'Syslog'],
+    'NETWORK': ['IPX', 'IPv4', 'IGMPv0', 'ICMPv6', 'ICMP', 'OSPF', 'PIMv0', 'IGRP', 'LPD', 'VRRP', 'RSL', 'ESP', 'GRE'],
+    'LINK': ['ARP', 'CDP', 'LLC', 'PPTP'],
+    'OTHER': ['NBNS', '0x200e', 'Gryphon', 'NBDS', 'NCP', 'UDPENCAP']
+}
+
+if __name__ == '__main__':
+    # read pcap csv dump
+    # (csv dump extracted from wireshark)
+    with open('univ1_trace.csv', 'rb') as f:
+        data = list(csv.reader(f))[1:]
+
+    protocols = {}
+    tcp = []
+    udp = []
+    ip = []
+
+    # collect all packet lengths
+    for no, time, source, destination, protocol, length, info in data:
+        if not protocol in protocols:
+            protocols[protocol] = int(length)
+        else:
+            protocols[protocol] += int(length)
+
+        # collect TCP lengths
+        if protocol == 'TCP':
+            tcp.append(length)
+        # collect UDP lengths
+        elif protocol == 'UDP':
+            udp.append(length)
+
+    # collect values
+    all_size = [x for x in protocols.values()]
+
+    # debug
+    # print(all_size)
+    print(tcp)
+    # print(udp)
+
+    ######################################################################
+
+    # plot all packets cdf
+    # sorted_list = np.sort(all_size)
+    # p = 1. * np.arange(len(all_size)) / (len(all_size) - 1)
+    # plt.plot(sorted_list, p)
+    # plt.xscale('log')
+    # plt.title('all packets')
+    # plt.show()
+
+    # plot tcp packets cdf
+    sorted_list = np.sort(tcp)
+    p = 1. * np.arange(len(tcp)) / (len(tcp) - 1)
+    plt.plot(sorted_list, p)
+    plt.xscale('log')
+    plt.title('tcp packets')
+    plt.show()
+
+    # plot udp packets cdf
+    # sorted_list = np.sort(udp)
+    # p = 1. * np.arange(len(udp)) / (len(udp) - 1)
+    # plt.plot(sorted_list, p)
+    # plt.xscale('log')
+    # plt.title('udp packets')
+    # plt.show()
---
+++ scripts/packet_type.py
@@ -0,0 +1,64 @@
+import csv
+
+mapping = {
+    'APPLICATION': ['TELNET', 'MySQL', 'SMTP', 'SSH', 'RIP', 'DHCPv6', 'DNS', 'CVSPSERVER', 'Intel ANS probe', 'VNC', 'SSL', 'ISAKMP', 'MDNS', 'MS NLB', 'DSI', 'LLMNR', 'NTP', 'NCS', 'BOOTP', 'SRVLOC'],
+    'TRANSPORT': ['TCP', 'UDP', 'NBSS', 'Syslog'],
+    'NETWORK': ['IPX', 'IPv4', 'IGMPv0', 'ICMPv6', 'ICMP', 'OSPF', 'PIMv0', 'IGRP', 'LPD', 'VRRP', 'RSL', 'ESP', 'GRE'],
+    'LINK': ['ARP', 'CDP', 'LLC', 'PPTP'],
+    'OTHER': ['NBNS', '0x200e', 'Gryphon', 'NBDS', 'NCP', 'UDPENCAP']
+}
+
+if __name__ == '__main__':
+    # read pcap csv dump
+    # (csv dump extracted from wireshark)
+    with open('univ1_trace.csv', 'rb') as f:
+        data = list(csv.reader(f))[1:]
+
+    protocols = {}
+
+    # coarse count of protocols
+    for no, time, source, destination, protocol, length, info in data:
+        if not protocol in protocols:
+            protocols[protocol] = {'count': 1, 'length': int(length)}
+        else:
+            protocols[protocol]['count'] += 1
+            protocols[protocol]['length'] += int(length)
+
+    # calculate total (for percentage calculation)
+    total = 0
+    for value in protocols.values():
+        total += value['count']
+
+    # output
+    print('protocol count:')
+    for key, value in protocols.items():
+        print(key + ':\t' + str(value['count']) + ' packets'
+                  + '\t' + str(value['length']) + ' bytes')
+    print('='*55)
+    print('link layer:')
+    link_count = 0
+    link_length = 0
+    for key, value in protocols.items():
+        if key in mapping['LINK']:
+            link_count += value['count']
+            link_length += value['length']
+    print(str(link_count) + ' packets\t' + '{0:.2f}'.format(float(link_count) / total * 100) + '%\t' + str(link_length) + ' bytes')
+    print('='*55)
+    print('network layer:')
+    network_count = 0
+    network_length = 0
+    for key, value in protocols.items():
+        if key in mapping['NETWORK']:
+            network_count += value['count']
+            network_length += value['length']
+    print(str(network_count) + ' packets\t' + '{0:.2f}'.format(float(network_count) / total * 100) + '%\t' + str(network_length) + ' bytes')
+    print('='*55)
+    print('transport layer:')
+    transport_count = 0
+    transport_length = 0
+    for key, value in protocols.items():
+        if key in mapping['TRANSPORT']:
+            transport_count += value['count']
+            transport_length += value['length']
+    print(str(transport_count) + ' packets\t' + '{0:.2f}'.format(float(transport_count) / total * 100) + '%\t' + str(transport_length) + ' bytes')
+    print('='*55)
