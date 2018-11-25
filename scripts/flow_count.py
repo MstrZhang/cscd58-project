e6fb51df308c6028ca1e371ac0672a76e13bf76c
zhangke <mstr.zhang@gmail.com>
2018-11-25 18:51:46 -0500

    code cleaning

--- scripts/flow_count.py
+++ scripts/flow_count.py
@@ -1,33 +1,19 @@
 import csv
 
-def ip_to_int(address):
-    split_address = address.split('.')
-    total = 0
-    multiplicator = 256^3
-    for item in split_address:
-        total += int(item) * multiplicator
-        multiplicator = multiplicator / 256
-    return total
-
 if __name__ == '__main__':
     # read pcap csv dump
     # (csv dump extracted from wireshark)
-    with open('../raw/univ1_trace.csv', 'rb') as f:
+    with open('../raw/flow_count_dump.csv', 'rb') as f:
         data = list(csv.reader(f))[1:]
 
     flows = {}
 
     # coarse count of flows
-    for no, time, source, destination, protocol, length, info in data:
+    for no, time, source, destination, protocol, length, info, tcp_src_port, tcp_dst_port, udp_src_port, udp_dst_port in data:
+        # Extract the src and dest ports
         if protocol == "UDP" or protocol == "TCP":
-            # Extract the src and dest ports
-            split_info = info.split('  >  ')
-            if not split_info[0][0].isdigit():
-                src_port = split_info[0].split(' ')[-1]
-                dest_port = split_info[1].split(' ')[0]
-            else:
-                src_port = split_info[0]
-                dest_port = split_info[1].split(' ')[0]
+            src_port = tcp_src_port if protocol == "TCP" else udp_src_port
+            dest_port = tcp_dst_port if protocol == "TCP" else udp_dst_port
 
             # build a flow identifier from src/dest ips
             ident = '{src_ip}:{src_port},{dst_ip}:{dst_port}'.format(src_ip=source, src_port=src_port, dst_ip=destination, dst_port=dest_port)
