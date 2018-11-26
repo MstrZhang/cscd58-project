e3d32d3302055a94d65c51049e13ad8fb23f5e2e
zhangke <mstr.zhang@gmail.com>
2018-11-25 19:45:34 -0500

    add more returns to flow count

--- scripts/flow_count.py
+++ scripts/flow_count.py
@@ -18,22 +18,48 @@ if __name__ == '__main__':
             # build a flow identifier from src/dest ips
             ident = '{src_ip}:{src_port},{dst_ip}:{dst_port}'.format(src_ip=source, src_port=src_port, dst_ip=destination, dst_port=dest_port)
             reverse = '{dst_ip}:{dst_port},{src_ip}:{src_port}'.format(src_ip=source, src_port=src_port, dst_ip=destination, dst_port=dest_port)
+            
             if (not ident in flows) and (not reverse in flows):
-                flows[ident] = {'protocol' : protocol}
+                flows[ident] = {'protocol': protocol, 'packets': 1, 'length': int(length)}
+            else:
+                try:
+                    flows[ident]['packets'] += 1
+                    flows[ident]['length'] += int(length)
+                except:
+                    flows[reverse]['packets'] += 1
+                    flows[reverse]['length'] += int(length)
 
-    # calculate total (for percentage calculation)
+    # calculate totals
     total = 0
     tcp_count = 0
     udp_count = 0
+    tcp_packet_count = 0
+    udp_packet_count = 0
+    tcp_length = 0
+    udp_length = 0
+
     for flow in flows:
         total += 1
         if flows[flow]['protocol'] == 'TCP':
             tcp_count += 1
+            tcp_packet_count += flows[flow]['packets']
+            tcp_length += flows[flow]['length']
         else:
             udp_count += 1
+            udp_packet_count += flows[flow]['packets']
+            udp_length += flows[flow]['length']
 
     # output
     print('flow count: ' + str(total) + ' flows')
-    print('TCP total: ' + ':\t' + str(tcp_count) + ' flows' + '\t' + '{0:.2f}'.format(float(tcp_count) / total * 100)) + '%'
-    print('UDP total: ' + ':\t' + str(udp_count) + ' flows' + '\t' + '{0:.2f}'.format(float(udp_count) / total * 100)) + '%'
+
+    print('TCP total:\t' + str(tcp_count) + ' flows\t' 
+                         + '{0:.2f}'.format(float(tcp_count) / total * 100) + '%\t'
+                         + str(tcp_packet_count) + ' packets\t'
+                         + str(tcp_length) + ' bytes')
+
+    print('UDP total:\t' + str(udp_count) + ' flows\t' 
+                     + '{0:.2f}'.format(float(udp_count) / total * 100) + '%\t'
+                     + str(udp_packet_count) + ' packets\t'
+                     + str(udp_length) + ' bytes')
+
     
