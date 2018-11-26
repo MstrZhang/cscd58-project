8bc2f2d699ac9f48031a9151fa3e8e54b3b64de1
zhangke <mstr.zhang@gmail.com>
2018-11-25 20:39:43 -0500

    modify flow size to use fixed flow calcluation and generate cdfs

--- scripts/flow_size.py
+++ scripts/flow_size.py
@@ -2,50 +2,47 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def ip_to_int(address):
-    split_address = address.split('.')
-    total = 0
-    multiplicator = 256^3
-    for item in split_address:
-        total += int(item) * multiplicator
-        multiplicator = multiplicator / 256
-    return total
-
 if __name__ == '__main__':
     # read pcap csv dump
     # (csv dump extracted from wireshark -- tcp and ip header lengths specifically extracted from wireshark)
-    with open('../raw/payload_dump.csv', 'rb') as f:
+    with open('../raw/flow_size.csv', 'rb') as f:
         data = list(csv.reader(f))[1:]
 
     flows = {}
 
     # collect all packet lengths
-    for no, time, source, destination, protocol, length, info, udp_payload, tcp_payload in data:
+    for no, time, source, destination, protocol, length, info, tcp_src_port, tcp_dst_port, tcp_payload, udp_src_port, udp_dst_port, syn in data:
+        # Extract the src and dest ports
         if protocol == "UDP" or protocol == "TCP":
-            # Extract the src and dest ports
-            split_info = info.split('  >  ')
-            if not split_info[0][0].isdigit():
-                src_port = split_info[0].split(' ')[-1]
-                dest_port = split_info[1].split(' ')[0]
-            else:
-                src_port = split_info[0]
-                dest_port = split_info[1].split(' ')[0]
+            src_port = tcp_src_port if protocol == "TCP" else udp_src_port
+            dest_port = tcp_dst_port if protocol == "TCP" else udp_dst_port
 
-            # Build a flow identifier from src/dest ips
-            ident = ip_to_int(source) + ip_to_int(destination) + int(src_port) + int(dest_port)
-            if not ident in flows:
-                flows[ident] = {'protocol' : protocol, 'packet-count' : 1, 'size' : int(length), 'header-size' : 0, 'payload-size' : 0}
-            else:
-                flows[ident]['packet-count'] += 1
-                flows[ident]['size'] += int(length)
+            # build a flow identifier from src/dest ips
+            ident = '{src_ip}:{src_port},{dst_ip}:{dst_port}'.format(src_ip=source, src_port=src_port, dst_ip=destination, dst_port=dest_port)
+            reverse = '{dst_ip}:{dst_port},{src_ip}:{src_port}'.format(src_ip=source, src_port=src_port, dst_ip=destination, dst_port=dest_port)
             
-            # One last check for overhead modifications. Only concerned with TCP
-            if flows[ident]['protocol'] == 'TCP':
-                if tcp_payload == '':
-                    tcp_payload = 0
-                flows[ident]['header-size'] += (int(length) - int(tcp_payload))
-                flows[ident]['payload-size'] += int(tcp_payload)
-    
+            if (not ident in flows) and (not reverse in flows):
+                flows[ident] = {'protocol': protocol, 'packet-count': 1, 'size': int(length), 'header-size': 0, 'payload-size': 0}
+            else:
+                try:
+                    flows[ident]['packet-count'] += 1
+                    flows[ident]['size'] += int(length)
+                    # check for overhead modifications; only concerned with TCP flows
+                    if flows[ident]['protocol'] == 'TCP':
+                        if tcp_payload == '':
+                            tcp_payload = 0
+                        flows[ident]['header-size'] += (int(length) - int(tcp_payload))
+                        flows[ident]['payload-size'] += int(tcp_payload)
+                except:
+                    flows[reverse]['packet-count'] += 1
+                    flows[reverse]['size'] += int(length)
+                    # check for overhead modifications; only concerned with TCP flows
+                    if flows[reverse]['protocol'] == 'TCP':
+                        if tcp_payload == '':
+                            tcp_payload = 0
+                        flows[reverse]['header-size'] += (int(length) - int(tcp_payload))
+                        flows[reverse]['payload-size'] += int(tcp_payload)
+
     # Extract the desired information
     packets_per_flow = []
     packets_per_TCP = []
@@ -85,7 +82,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(packets_per_flow)) / (len(packets_per_flow) - 1)
     plt.plot(sorted_list, p)
     plt.xscale('log')
-    plt.title('combined flow sizes (packets)')
+    plt.title('CDF of Combined Flow Sizes (packets)')
 
     # plot all flow bytes cdf
     plt.figure(2)
@@ -93,7 +90,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(size_per_flow)) / (len(size_per_flow) - 1)
     plt.plot(sorted_list, p)
     plt.xscale('log')
-    plt.title('combined flow sizes (bytes)')
+    plt.title('CDF of Combined Flow Sizes (bytes)')
 
     # plot tcp flow packets cdf
     plt.figure(3)
@@ -101,7 +98,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(packets_per_TCP)) / (len(packets_per_TCP) - 1)
     plt.plot(tcp_list, p)
     plt.xscale('log')
-    plt.title('tcp flow sizes (packets)')
+    plt.title('CDF of TCP Flow Sizes (packets)')
 
     # plot tcp flow bytes cdf
     plt.figure(4)
@@ -109,7 +106,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(size_per_TCP)) / (len(size_per_TCP) - 1)
     plt.plot(sorted_list, p)
     plt.xscale('log')
-    plt.title('tcp flow sizes (packets)')
+    plt.title('CDF of TCP Flow Sizes (bytes)')
 
     # plot udp flow packets cdf
     plt.figure(5)
@@ -117,7 +114,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(packets_per_UDP)) / (len(packets_per_UDP) - 1)
     plt.plot(udp_list, p)
     plt.xscale('log')
-    plt.title('udp flow sizes (packets)')
+    plt.title('CDF of UDP Flow Sizes (packets)')
 
     # plot udp flow bytes cdf
     plt.figure(6)
@@ -125,7 +122,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(size_per_UDP)) / (len(size_per_UDP) - 1)
     plt.plot(sorted_list, p)
     plt.xscale('log')
-    plt.title('udp flow sizes (bytes)')
+    plt.title('CDF of UDP Flow Sizes (bytes)')
 
     # ######################################################################
     # # plot tcp overhead ratio cdf
@@ -137,7 +134,7 @@ if __name__ == '__main__':
     p = 1. * np.arange(len(TCP_overhead_ratios)) / (len(TCP_overhead_ratios) - 1)
     plt.plot(tcp_header_list, p)
     plt.xscale('log')
-    plt.title('tcp overhead ratios')
+    plt.title('CDF of TCP Overhead Ratios')
 
     # show figures
     plt.show()
